9328be078529c7e3fe05b4ddc09e3fd962836bd4
yui-kuta <137240438+yui-kuta@users.noreply.github.com>
2023-08-22 21:08:05 +0900

    added kadai_019

---
+++ kadai_019/event.js
@@ -0,0 +1,8 @@
+
+const outputBtn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+
+outputBtn.addEventListener('click', () => {
+  text.textContent = 'ボタンをクリックしました';
+});
